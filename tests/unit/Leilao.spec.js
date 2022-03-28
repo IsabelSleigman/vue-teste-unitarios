43519934e492641f6c8ec3265595ddd9ebe5f069
isabel.sleigman <sleimusic@gmail.com>
2022-03-28 16:34:59 -0300

    ajustes

---
+++ tests/unit/Leilao.spec.js
@@ -0,0 +1,7 @@
+import Leilao from '@/components/Leilao'
+import { mount } from '@vue/test-utils'
+describe('Um leilão exibe os dados do produto', () => {
+  test('exibe os dados do leilão no card', () => {
+    expect(false).toBe(true)
+  })
+})
