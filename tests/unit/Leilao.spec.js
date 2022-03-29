b82291ee784701fcd843482aeaac054477c2cee0
isabel.sleigman <sleimusic@gmail.com>
2022-03-29 14:13:40 -0300

    leilão

--- tests/unit/Leilao.spec.js
+++ tests/unit/Leilao.spec.js
@@ -1,7 +1,26 @@
 import Leilao from '@/components/Leilao'
 import { mount } from '@vue/test-utils'
-describe('Um leilão exibe os dados do produto', () => {
+
+const leilao = {
+  produto: 'Livro da Casa do Código',
+  lanceInicial: 50,
+  descricao: 'Livro sobre VueJS'
+}
+
+describe('Um leilão é exibido corretamente', () => {
   test('exibe os dados do leilão no card', () => {
-    expect(false).toBe(true)
+    const wrapper = mount(Leilao, {
+      propsData: {
+        leilao
+      }
+    })
+
+    const header = wrapper.find('.card-header').element
+    const title = wrapper.find('.card-title').element
+    const text = wrapper.find('.card-text').element
+
+    expect(header.textContent).toContain(`Estamos leiloando um(a): ${leilao.produto}`)
+    expect(title.textContent).toContain(`Lance inicial: R$ ${leilao.lanceInicial}`)
+    expect(text.textContent).toContain(leilao.descricao)
   })
 })
