860629996c37a3d086d953ac19d324aef6fe02fe
isabel.sleigman <sleimusic@gmail.com>
2022-03-29 14:41:49 -0300

    criado reste novo leilão

---
+++ tests/unit/NovoLeilao.spec.js
@@ -0,0 +1,29 @@
+import NovoLeilao from '@/views/NovoLeilao'
+import { mount } from '@vue/test-utils'
+import { createLeilao } from '@/http'
+import flushPromises from 'flush-promises'
+
+jest.mock('@/http')
+
+const $router = {
+  push: jest.fn()
+}
+
+describe('Um novo leilao deve ser criado', () => {
+  test('dado o formulario preenchido, um novo leilao deve ser criado', () => {
+    createLeilao.mockResolvedValueOnce()
+
+    const wrapper = mount(NovoLeilao, {
+      mocks: {
+        $router
+      }
+    })
+
+    wrapper.find('.produto').setValue('Um livro da cada do código')
+    wrapper.find('.descricao').setValue('Um livro da hora')
+    wrapper.find('.valor').setValue(50)
+    wrapper.find('form').trigger('submit')
+
+    expect(createLeilao).toHaveBeenCalled()
+  })
+})
